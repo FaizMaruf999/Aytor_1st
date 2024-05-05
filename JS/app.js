9ca45393752a92fd1fe692e677d58d4bc3c9f93e
FaizMaruf999 <152320043+FaizMaruf999@users.noreply.github.com>
2024-05-06 02:36:23 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+    // SLICK SLIDER (BANNER SLIDER)
+    $(".Bannerslider").slick({
+        arrows: false,
+        dots: true,
+        dotsClass: 'bannerDots container',
+    });
+
+  });
